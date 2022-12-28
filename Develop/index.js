525a43a648bd6230d3ba33a66684af3e6210f9d4
schrolan <schrolan000@gmail.com>
2022-12-27 19:16:28 -0600

    added the questions that will be asked to generate the README.

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,46 @@
 // TODO: Include packages needed for this application
-
+import inquirer from "inquirer";
 // TODO: Create an array of questions for user input
-const questions = [];
+//These are the question to pick from when creating the README
+const questions = [
+    {
+    type: "input",
+    name: "title",
+    message: "Please choose a title for your project:",
+    default: "My README."
+},
+{
+    type: "input",
+    name: "description",
+    mesage: "Please write a description of the project:",
+    default: "Your description goes here."
+},
+{
+    type: "input",
+    name: "table of contents",
+    message: "Please write a table of contents:",
+    default: "A table of contents goes here."
+},
+{
+    type: "input",
+    name: "installation",
+    message: "Please put the steps for installation here:",
+    default: "Installation steps goes here."
+},
+{
+    type: "input",
+    name: "usage information",
+    message: "Please write your usage information here:",
+    default: "Usage information goes here."
+},
+{
+    type: "list",
+    name: "licenses",
+    message: "Which license fits your project?",
+    default: "Apache 2.0 License",
+    choices: ["Apache 2.0 License", "Boost Software License 1.0", "CC0", "Eclipse Public License 1.0", "GNU GPL v3", "The Hippocratic License 2.1", "IBM Public License Version 1.0", "The MIT License", "Mozilla Public License 2.0", "Open Database License (ODbL)", "The Artistic License 2.0", "SIL Open Font License 1.1", "The Unlicense", "The zlib/libpng License"]
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
