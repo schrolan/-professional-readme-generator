70d0da804ae6ac0a4c2b5b0bc97cfa3e6cbe8168
schrolan <schrolan000@gmail.com>
2022-12-30 19:33:16 -0600

    Finished the questions a user must answer to complete the README.

--- Develop/index.js
+++ Develop/index.js
@@ -39,7 +39,31 @@ const questions = [
     message: "Which license fits your project?",
     default: "Apache 2.0 License",
     choices: ["Apache 2.0 License", "Boost Software License 1.0", "CC0", "Eclipse Public License 1.0", "GNU GPL v3", "The Hippocratic License 2.1", "IBM Public License Version 1.0", "The MIT License", "Mozilla Public License 2.0", "Open Database License (ODbL)", "The Artistic License 2.0", "SIL Open Font License 1.1", "The Unlicense", "The zlib/libpng License"]
-}
+},
+{
+    type: "input",
+    name: "contribution",
+    message: "Please write how others can contribute here:",
+    default: "Contribution information goes here."
+},
+{
+    type: "input",
+    name: "tests",
+    message: "Please write how to run tests for this project here:",
+    default: "Test information goes here."
+},
+{
+    type: "input",
+    name: "username",
+    message: "Please write your github username here:",
+    default: "Username information goes here."
+},
+{
+    type: "input",
+    name: "usage information",
+    message: "Please write the email associated with your github here:",
+    default: "Github associated email goes here."
+},
 ];
 
 // TODO: Create a function to write README file
